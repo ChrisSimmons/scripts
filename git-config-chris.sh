a7b922082e142491e75157121c10dccf3386c8d5
Chris Simmons <chris.dev@netchris.com>
2024-03-30 16:26:22 -0700

    git-config-chris.sh

---
+++ git-config-chris.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# git configuration for Chris
+# $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/system-bootstrapping/main/git-config-chris.sh)
+
+git config --global user.email "chris.dev@netchris.com"
+git config --global user.name "Chris Simmons"
