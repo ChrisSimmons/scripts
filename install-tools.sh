73c1a823954ba46d4fea16dee86ed9dddf55ece6
Chris Simmons <chris.dev@netchris.com>
2024-03-30 16:36:36 -0700

    Update tools install

--- install-tools.sh
+++ install-tools.sh
@@ -2,4 +2,19 @@
 # Install required tools
 # $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/system-bootstrapping/main/install-tools.sh)
 
-sudo apt install git jq
+echo "Updating apt ..."
+sudo apt update
+
+echo "Installing git ..."
+sudo apt install git -y
+
+echo "Installing jq ..."
+sudo apt install jq -y
+
+echo "Installing GitHub CLI ..."
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md#installing-gh-on-linux-and-bsd
+sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
