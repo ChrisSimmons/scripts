05e472b127648bca57686740555b9ccc06bee582
Chris Simmons <chris.dev@netchris.com>
2024-03-30 16:31:27 -0700

    init install-tools.sh

---
+++ install-tools.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Install required tools
+# $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/system-bootstrapping/main/install-tools.sh)
+
+sudo apt install git jq
