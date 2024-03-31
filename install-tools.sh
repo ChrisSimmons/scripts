f52be6196634aa91c7f02b1b62217df350ec35da
Chris Simmons <chris.dev@netchris.com>
2024-03-30 18:21:34 -0700

    Add yadm to tools

--- install-tools.sh
+++ install-tools.sh
@@ -8,11 +8,16 @@ sudo apt update
 echo "Installing git ..."
 sudo apt install git -y
 
+echo "Installing yadm ..."
+sudo apt install yadm -y
+
 echo "Installing jq ..."
 sudo apt install jq -y
 
 echo "Installing GitHub CLI ..."
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md#installing-gh-on-linux-and-bsd
+# You may find some duplication of commands here compared to above (e.g. "sudo apt update") but just keep this as-is
+# as I just want to be able to go to the link above, copy the current command, and then paste it below.
 sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
