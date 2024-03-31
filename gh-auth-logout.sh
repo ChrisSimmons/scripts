dcea54b6e7c05de0bd399a9b86423791695bc3f7
Chris Simmons <chris.dev@netchris.com>
2024-03-30 17:05:11 -0700

    gh-auth-logout.sh

---
+++ gh-auth-logout.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# GitHub CLI logout (for cleanup after pushing up SSH key)
+# $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/system-bootstrapping/main/gh-auth-logout.sh)
+
+gh auth logout
