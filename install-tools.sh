96477fc10c818e5de91bc1cb417ca94e8a6c3a96
Chris Simmons <gitlab.com@netchris.com>
2024-04-03 15:26:54 -0700

    tools + net-tools

--- install-tools.sh
+++ install-tools.sh
@@ -29,3 +29,6 @@ sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packa
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+
+echo "Installing net-tools ..."
+sudo apt install net-tools -y
