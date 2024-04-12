2494f48c81bc3f328478be6d6829d8690129affc
Chris Simmons <chris.dev@netchris.com>
2024-04-12 13:32:18 -0700

    Functions stub

---
+++ unix-like/functions.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Helpful functions.  Source in your code as follows: 
+# $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/scripts/main/unix-like/functions.sh)
+
+hello_world() {
+  echo "Hello world!"
+}
