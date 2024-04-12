d2d322b45c37132411e030a6286d6cfa266ad0d8
Chris Simmons <chris.dev@netchris.com>
2024-04-12 22:28:02 +0100

    WIP

--- unix-like/deleteme.sh
+++ unix-like/deleteme.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # TODO - DELETE THIS BEFORE RELEASE
 
-source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/scripts/generalize/unix-like/functions.sh)
+source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/scripts/generalize/unix-like/functions.sh?xxx)
 
 establish_os_ver
 
