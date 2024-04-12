281800544b3b5c548490efc17caeea35505ed5bc
Chris Simmons <chris.dev@netchris.com>
2024-04-12 13:29:07 -0700

    TODO

--- unix-like/add-self-to-sudoers.sh
+++ unix-like/add-self-to-sudoers.sh
@@ -2,6 +2,8 @@
 # Add self to sudoers and lift password requirement
 # $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/scripts/main/unix-like/add-self-to-sudoers.sh)
 
+# TODO - Detect raspbian and run remove-pi-sudoers.sh
+
 # Configure to allow $USER to sudo without password
 export SUDOERS_NO_PASSWORD=/etc/sudoers.d/$USER
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee $SUDOERS_NO_PASSWORD
