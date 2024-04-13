b8f62855222b875a298134596525e0221f8e3d59
Chris Simmons <chris.dev@netchris.com>
2024-04-13 02:32:09 +0100

    Comment

--- unix-like/generate-ssh-key.sh
+++ unix-like/generate-ssh-key.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Generate the SSH key
 # THIS SCRIPT IS NOT IDEMPOTENT:
-# While it can be run over and over safely, it will wipe out the existing SSH key.  It should be run with this understanding.
+# While it can be run over and over safely, it will wipe out the existing SSH key.  If you regenerate the key
+# you will need to re-add the public key to servers to which you need to authenticate.
 # $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/scripts/main/unix-like/generate-ssh-key.sh)
 
 SSH_KEY_TITLE="$USER@$HOSTNAME (`date +%s`)"
