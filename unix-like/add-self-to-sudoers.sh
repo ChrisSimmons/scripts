58306f51538048631abe68cf68c39d2ed0c97703
Chris Simmons <chris.dev@netchris.com>
2024-04-13 02:33:01 +0100

    Comment

--- unix-like/add-self-to-sudoers.sh
+++ unix-like/add-self-to-sudoers.sh
@@ -2,7 +2,7 @@
 # Add self to sudoers and lift password requirement
 # $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/scripts/main/unix-like/add-self-to-sudoers.sh)
 
-# On Raspberry Pi the "/etc/sudoers.d/010_pi-nopasswd" is created, but we want to rid ourselves of that.
+# On Raspberry Pi the "/etc/sudoers.d/010_pi-nopasswd" file is created, but we want to rid ourselves of that.
 sudo rm -f /etc/sudoers.d/010_pi-nopasswd
 
 # Configure to allow $USER to sudo without password
