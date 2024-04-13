0b088ebdb7a61f75718df384b94f614d308d4371
Chris Simmons <chris.dev@netchris.com>
2024-04-13 01:00:46 +0100

    DELETE

--- unix-like/deleteme.sh
+++ unix-like/deleteme.sh
@@ -12,23 +12,3 @@ echo "VER: $VER"
 echo "VALUES_FOUND_IN: $VALUES_FOUND_IN"
 echo "Epoch (Unix time): $(unix_time)"
 echo "OS_FIRST: $OS_FIRST"
-
-
-case $OS_FIRST in
-
-  DEBIAN)
-    echo "This is Debian"
-    ;;
-
-  UBUNTU)
-    echo "This is Ubuntu"
-    ;;
-
-  DARWIN)
-    echo "This is Mac OS"
-    ;;
-
-  *)
-    echo "This is something else"
-    ;;
-esac
