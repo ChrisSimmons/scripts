bf57c0c7e799b08916b6a3d23fa180e800268e48
Chris Simmons <chris.dev@netchris.com>
2026-04-30 19:11:02 -0700

    Retire

--- unix-like/gh-auth-login.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# GitHub CLI login
-# $ source <(curl -s https://raw.githubusercontent.com/ChrisSimmons/scripts/main/unix-like/gh-auth-login.sh)
-
-echo "Auth/Login for GitHub CLI.  Please \"Skip\" uploading your SSH key.  We'll do that later..."
-gh auth login --git-protocol ssh --web --scopes "admin:public_key"
